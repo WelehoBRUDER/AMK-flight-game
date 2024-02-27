194dd5b580b169974314ec608efe7d3d620bd91c
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-02-27 11:30:21 +0200

    incomplete airport origin function
    please do not look at what im cooking

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import os
 import geopy.distance
 import mysql.connector
+import random
 from dotenv import load_dotenv
 
 db = {}
@@ -19,8 +20,6 @@ def connect_to_db():
         'password': os.getenv('PASSWORD'),
         'database': os.getenv('DATABASE')
     }
-    # Debug to see if .env is working
-    print(db_variables)
 
     # Connect to SQL-database using the .env variables
     db["database"] = mysql.connector.connect(**db_variables)
@@ -33,6 +32,24 @@ def get_some_airports():
     return db["cursor"].fetchall()
 
 
+# This function returns 16 random airports, 2 in each direction
+def draw_airports_from_origin(origin):
+    flight_bearings = (0, 45, 90, 135, 180, 225, 270, 315)
+    ports = []
+    # origin_coords = {'lat': origin["latitude_deg"], 'lon': origin["longitude_deg"]}
+    dist = (600, 1000)
+    for bearing in flight_bearings:
+        distance = random.randint(dist[0], dist[1])
+        destination = geopy.distance.distance(miles=distance).destination((34, 148), bearing=bearing)
+        ports.append(destination)
+    print(ports)
+    for port in ports:
+        db["cursor"].execute(
+            f"SELECT TOP 2 * FROM airport WHERE latitude_deg = {port[0]} AND longitude_deg = {port[1]} ORDER BY ABS({port[0]} - latitude_deg)")
+        airport_data = db["cursor"].fetchmany(2)
+        print(airport_data)
+
+
 def distance_between_airports(port_1, port_2):
     db["cursor"].execute(
         f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{port_1}' OR ident = '{port_2}'")
@@ -42,5 +59,6 @@ def distance_between_airports(port_1, port_2):
 
 
 connect_to_db()
-print(distance_between_airports("EFHK", "EFIV"))
-print(get_some_airports())
+# print(distance_between_airports("EFHK", "EFIV"))
+# print(get_some_airports())
+# draw_airports_from_origin({})
