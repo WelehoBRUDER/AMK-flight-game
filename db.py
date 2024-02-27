deda80a12a1758cb2ee292883e9021e244cd1e71
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-02-27 09:52:41 +0200

    reformatted db

--- db.py
+++ db.py
@@ -3,29 +3,37 @@ import geopy.distance
 import mysql.connector
 from dotenv import load_dotenv
 
-# Loads .env to current os.environ
-load_dotenv()
+db = {}
 
-# Load database credentials from .env
-# This is to prevent having to commit sensitive information
-# like password and username
-db_variables = {
-    'host': os.getenv('HOST'),
-    'user': os.getenv('USER'),
-    'password': os.getenv('PASSWORD'),
-    'database': os.getenv('DATABASE')
-}
-# Debug to see if .env is working
-print(db_variables)
 
-# Connect to SQL-database using the .env variables
-db = mysql.connector.connect(**db_variables)
-cursor = db.cursor()
+def connect_to_db():
+    # Loads .env to current os.environ
+    load_dotenv()
+
+    # Load database credentials from .env
+    # This is to prevent having to commit sensitive information
+    # like password and username
+    db_variables = {
+        'host': os.getenv('HOST'),
+        'user': os.getenv('USER'),
+        'password': os.getenv('PASSWORD'),
+        'database': os.getenv('DATABASE')
+    }
+    # Debug to see if .env is working
+    print(db_variables)
+
+    # Connect to SQL-database using the .env variables
+    db["database"] = mysql.connector.connect(**db_variables)
+    db["cursor"] = db["database"].cursor()
 
 
 def distance_between_airports(port_1, port_2):
-    cursor.execute(
+    db["cursor"].execute(
         f"SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '{port_1}' OR ident = '{port_2}'")
-    airport_data = cursor.fetchmany(2)
+    airport_data = db["cursor"].fetchmany(2)
     distance = geopy.distance.distance(airport_data[0], airport_data[1])
     return distance
+
+
+connect_to_db()
+print(distance_between_airports("EFHK", "EFIV"))
