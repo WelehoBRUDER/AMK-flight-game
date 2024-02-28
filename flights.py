8f80ce0bd5c762f02f8968fdc5b501291be413ad
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2024-02-28 15:57:43 +0200

    added a flight times function

--- flights.py
+++ flights.py
@@ -1,5 +1,28 @@
 from db import get_some_airports
+import random
+from colorama import Fore
 
 
 def flights():
     airports = get_some_airports()
+    return
+
+
+def times_of_the_flights():  # Arpoo satunnaisia aikoja lennoille.
+    flight_times = []
+    for i in range(16):
+        random_hours = random.randint(00, 24)
+        random_minutes = random.randint(00, 59)
+        flight_times.append((random_hours, random_minutes))
+        flight_times.sort()
+    return flight_times
+
+
+print(f"{Fore.YELLOW}DEPARTURES")
+print(f"Options     Time     Destination         Cost       Direction       Range")
+
+flight_times = times_of_the_flights()
+for i, (hours, minutes) in enumerate(flight_times, start=1):  # Listataan lähtevien lentojen tietoja allekkain.
+    print(
+        f"{i:02d}         {hours:02d}:{minutes:02d}     Destination{i:02d}       Cost{i:02d}     "
+        f"Direction{i:02d}     Range{i:02d}")
