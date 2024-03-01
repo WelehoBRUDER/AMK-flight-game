bd14308d3fac04064264be964a612fe88ddfa18f
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-03-01 15:29:46 +0200

    added a direction to flights and more comments to draw_airports_from_origin()

--- db.py
+++ db.py
@@ -34,30 +34,42 @@ def get_some_airports():
 
 # This function returns 16 random airports, 2 in each direction
 # The origin point is the latitude and longitude of the current airport
+# The return value is a list of dictionaries with the keys "flight_direction" and "airport"
+# [{"flight_direction: "North-West", "airport": {...}}, ...]
 def draw_airports_from_origin(lat, lon):
     # North, North-East, East, South-East, South, South-West, West, North-West
     flight_bearings = (0, 45, 90, 135, 180, 225, 270, 315)
+    bearings_text = ("North", "North-East", "East", "South-East", "South", "South-West", "West", "North-West")
     flight_points = []
-    ports = []
+    flights = []
     # Minimum and maximum distance from current airport in miles
     min_dist = 250
     max_dist = 850
-    for bearing in flight_bearings:
+    for i in range(len(flight_bearings)):
+        bearing = flight_bearings[i]
+        # Randomly pick distance to travel to
         distance = random.randint(min_dist, max_dist)
         # Get latitude and longitude of randomly selected place using the origin point
         destination = geopy.distance.distance(miles=distance).destination((lat, lon), bearing=bearing)
-        flight_points.append(destination)
+        # Add flight point and the point's direction
+        flight_points.append({"dest": destination, "dir": bearings_text[i]})
     # Find two airports per flight point
     for point in flight_points:
-        lat, lon = point[0], point[1]
+        # Gets the latitude and longitude of the desired point
+        lat, lon = point["dest"][0], point["dest"][1]
+        # Request two airports that are as close as possible to the point
+        # This is done by adding the latitude and longitude together and sorting the absolute value
         db["cursor"].execute(
             f"SELECT * FROM airport ORDER BY ABS({lat} - latitude_deg) + ABS({lon} - longitude_deg) LIMIT 2;")
+        # This could also be fetchall() since the query is limited to 2
+        # But things might break if somehow more were to slip past
         airport_data = db["cursor"].fetchmany(2)
+        # If airports were found, add them to the list that will be returned
         if airport_data:
             for airport in airport_data:
-                ports.append(airport)
+                flights.append({"flight_direction": point["dir"], "airport": airport})
 
-    return ports
+    return flights
 
 
 def distance_between_airports(port_1, port_2):
@@ -71,4 +83,4 @@ def distance_between_airports(port_1, port_2):
 connect_to_db()
 # print(distance_between_airports("EFHK", "EFIV"))
 # print(get_some_airports())
-# print(draw_airports_from_origin(34, 130))
+print(draw_airports_from_origin(34, 130))
