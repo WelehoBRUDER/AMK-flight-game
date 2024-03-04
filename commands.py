78f0f8b9cfd3c8ebba806d4e90b87821fd2dd664
Timppav <tino.vuorela@gmail.com>
2024-03-04 15:14:37 +0200

    Commands are now called from a dictionary instead of using elif-statements

--- commands.py
+++ commands.py
@@ -9,22 +9,24 @@ from db import draw_airports_from_origin
 def command(text):
     if text in commands:
         return commands[text]
-    if text == "help help":
-        return command_list["help"]
+    elif text == "help help":
+        return help_list["help"]
     elif text == "help status":
-        return command_list["status"]
+        return help_list["status"]
     elif text == "help fly":
-        return command_list["fly"]
+        return help_list["fly"]
     elif text == "help exit":
-        return command_list["exit"]
+        return help_list["exit"]
     else:
         return "Invalid command!"
 
 
 # Prints everything in the command_list dictionary
 def command_help():
-    for c in command_list.values():
-        print(c)
+    text = ""
+    for line in help_list.values():
+        text += "\n" + line
+    return text
 
 
 # Testing a function that handles changing the players' location
@@ -32,29 +34,32 @@ def command_help():
 def location_change():
     new_location = int(input("Select the flight you want to take (1-16): "))
     if new_location in airport_dict.keys():
-        stats[2] = f"Airport {new_location}"
-        print(f"Location changed to {new_location}")
+        status[2] = f"Airport {new_location}"
+        return f"Location changed to {new_location}"
     else:
-        print("Location not found!")
-
-
-# Player stats: 1 = Player name, 2 = Location, 3 = Money, 4 = CO2, 5 = Day, 6 = Hours, 7 = Minutes
-# The hours and minutes can probably be done a lot better
-stats = {1: "Player", 2: "Location", 3: 0,
-         4: 0, 5: 0, 6: 0, 7: 0}
+        return "Flight not found!"
 
 
 # A dictionary of all commands and a short explanation for each one
-command_list = {"help": f"{Fore.GREEN}Help{Style.RESET_ALL} - Shows this list. Typing a command after "
-                        f"'help' will give you the description for that specific command.",
-                "status": f"{Fore.GREEN}Status{Style.RESET_ALL} - "
-                          f"Shows your name, location, money, consumed CO2, days and time",
-                "fly": f"{Fore.GREEN}FLy{Style.RESET_ALL} - Fly to specified airport",
-                "exit": f"{Fore.GREEN}Exit{Style.RESET_ALL} - Quits the game"}
+help_list = {"help": f"{Fore.GREEN}Help{Style.RESET_ALL} - Shows this list. Typing a command after "
+                     f"'help' will give you the description for that specific command.",
+             "status": f"{Fore.GREEN}Status{Style.RESET_ALL} - "
+                       f"Shows your name, location, money, consumed CO2, days and time",
+             "fly": f"{Fore.GREEN}FLy{Style.RESET_ALL} - Fly to specified airport",
+             "exit": f"{Fore.GREEN}Exit{Style.RESET_ALL} - Quits the game"}
 
-# Defines what each command does
-# "Fly" and "exit" aren't here for now, because I couldn't get them to work inside the dictionary
-commands = {"status": stats}
+# Contains all the commands that use functions
+command_functions = {1: command_help, 2: location_change}
+# Refers to the functions in 'command_functions'
+help_command = command_functions[1]()
+fly_command = command_functions[2]
+# Player stats: 1 = Player name, 2 = Location, 3 = Money, 4 = CO2, 5 = Day, 6 = Hours, 7 = Minutes
+# The hours and minutes can probably be done a lot better
+status = {1: "Player", 2: "Location", 3: 0,
+          4: 0, 5: 0, 6: 0, 7: 0}
+# Contains all the commands and defines what each one does
+# Calling the functions directly here doesn't work, that's why it refers to the variables instead as a workaround
+commands = {"help": help_command, "status": status, "fly": fly_command}
 
 # Function imported from db.py. Returns 16 airports from selected latitude and longitude
 list_of_airports = draw_airports_from_origin(34, 130)
@@ -73,9 +78,5 @@ while True:
     command_input = input("\nEnter a command: ").lower()
     if command_input == "exit":
         break
-    elif command_input == "help":
-        command_help()
-    elif command_input == "fly":
-        location_change()
     else:
-        print(f"{command(command_input)}")
+        print(command(command_input))
