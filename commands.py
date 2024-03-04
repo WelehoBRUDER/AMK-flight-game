b7d7437fe02d2ed0435f690a0b8a7ed149cdf1d5
Timppav <tino.vuorela@gmail.com>
2024-03-04 16:30:33 +0200

    The "fly" command now prints all available flights.

--- commands.py
+++ commands.py
@@ -4,42 +4,46 @@ from db import draw_airports_from_origin
 
 # Function that checks whether the given command is found in the list of commands, and then returns the
 # corresponding text or function
-# Also returns the description for a command when "help" is written in front of it
-# Pretty crude way of coding it (for now) but it works
 def command(text):
-    if text in commands:
-        return commands[text]
-    elif text == "help help":
-        return help_list["help"]
-    elif text == "help status":
-        return help_list["status"]
-    elif text == "help fly":
-        return help_list["fly"]
-    elif text == "help exit":
-        return help_list["exit"]
+    if text in command_functions:
+        return command_functions[text]
     else:
-        return "Invalid command!"
+        return invalid_command
 
 
-# Prints everything in the command_list dictionary
+# Prints the description for every command
 def print_helplist():
     for line in help_list.values():
         print(line)
 
 
+# Prints the players' status
 def print_status():
     print(status)
 
 
 # Testing a function that handles changing the players' location
 # The updated location should show up when using the "status" command
-def location_change():
-    new_location = int(input("Select the flight you want to take (1-16): "))
+def fly():
+    # Function imported from db.py. Returns 16 airports from selected latitude and longitude
+    list_of_airports = draw_airports_from_origin(34, 130)
+    airport_num = 1
+    airport_dict = {}
+    # Lists every airport available, and assigns a number to each one
+    for airport in list_of_airports:
+        print(airport_num, airport)
+        airport_dict[airport_num] = airport
+        airport_num += 1
+    new_location = int(input("\nSelect the flight you want to take (1-16): "))
     if new_location in airport_dict.keys():
         status[2] = f"Airport {new_location}"
         print(f"Location changed to {new_location}")
     else:
         print("Flight not found!")
+
+
+def invalid_command():
+    print("Invalid command!")
 
 
 # A dictionary of all commands and a short explanation for each one
@@ -56,25 +60,7 @@ status = {1: "Player", 2: "Location", 3: 0,
           4: 0, 5: 0, 6: 0, 7: 0}
 
 # Contains all the commands that use functions
-command_functions = {1: print_helplist, 2: print_status, 3: location_change}
-# Refers to the functions in 'command_functions'
-help_command = command_functions[1]
-status_command = command_functions[2]
-fly_command = command_functions[3]
-# Contains all the commands and defines what each one does
-# Calling the functions directly here doesn't work, that's why it refers to the variables instead as a workaround
-commands = {"help": help_command, "status": status_command, "fly": fly_command}
-
-# Function imported from db.py. Returns 16 airports from selected latitude and longitude
-list_of_airports = draw_airports_from_origin(34, 130)
-airport_num = 1
-airport_dict = {}
-
-# Lists every airport available, and assigns a number to each one
-for airport in list_of_airports:
-    print(airport_num, airport)
-    airport_dict[airport_num] = airport
-    airport_num += 1
+command_functions = {"help": print_helplist, "status": print_status, "fly": fly}
 
 # Keeps asking the player for a command until they exit
 # This is here for testing purposes, it'll be removed once this file gets imported to main.py
