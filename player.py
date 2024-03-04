c927dbdcedf02df7d4176a676e61d8fe52d67f13
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-03-04 14:30:58 +0200

    added function to insert new player

---
+++ player.py
@@ -0,0 +1,8 @@
+"""
+    player data structure:
+        id: id of the player
+        screen_name: name of the player
+        co2_consumed: how much co2 the player has produced
+        location: location of the player (airport)
+        money: how broke the player is
+"""
