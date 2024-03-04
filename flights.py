e391a250998f307b40442c1951f5a5d9425c464a
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2024-03-04 14:51:02 +0200

    kaikki on rikki

--- flights.py
+++ flights.py
@@ -3,21 +3,19 @@ import random
 from colorama import Fore
 
 
-def flights():
-    airports = get_some_airports()
-    return
-
-
-def times_of_the_flights():  # Draws random times for flights and adds them to the list.
+def times_of_the_flights(flights):  # Draws random times for flights and adds them to the list.
     flight_times = []
     # Assigns an index to randomly generated times. Index starts from 1 instead of 0.
     for i, time in enumerate(range(1, 17), start=1):
         random_hours = random.randint(00, 23)
         random_minutes = random.randint(00, 59)
-        flight_times.append((i, random_hours, random_minutes))
+        flight_times.append(flights[i])
+        print(flights)
+        print(flight_times[i - 1])
+        flight_times[i - 1]["time"] = [random_hours, random_minutes]
 
     # Sorts the list by hours and minutes instead of their index.
-    flight_times.sort(key=lambda h: (h[1], h[2]))
+    flight_times.sort(key=lambda flight: (flight["time"][0], flight["time"][1]))
     return flight_times
 
 
@@ -26,35 +24,45 @@ def airport_names():  # Collects the possible flights from database and lists th
     destinations = draw_airports_from_origin(34, 130)
     # Assigns an index to destinations collected from database. Index starts from 1 instead of 0.
     for a, destination in enumerate(destinations, start=1):
-        municipalities = destination["municipality"]
+        print(destination)
+        airport = destination["airport"]
+        municipalities = airport["municipality"]
         destination_names.append((a, municipalities))
     return destination_names
 
 
-def flight_timetable():  # Prints a flight table with options for the player.
+def flight_direction():
+    direction_names = []
+    flight_directions = draw_airports_from_origin(34, 130)
+    for di, direction in enumerate(flight_directions, start=1):
+        destination_direction = direction["flight_direction"]
+        #directions = direction["dir"]
+        direction_names.append((di, destination_direction))
+    return direction_names
+
+
+def flight_timetable():  # Prints a flight timetable with options for the player.
     print(f"{Fore.YELLOW}DEPARTURES")
     print(f"Options     Time     Destination                Cost       Direction       Range")
 
-    # Creates a separate counter for Options.
+    # Creates a separate counter for Player Options.
     options = 1
 
     # Generates a list with information from above functions
-    flight_times = times_of_the_flights()
     destination_names = airport_names()
+    flight_times = times_of_the_flights(destination_names)
+    direction_names = flight_direction()
     # Searches information about the flights from lists compiled in above functions and prints them like a timetable.
-    for flight_info, destination_info in zip(flight_times, destination_names):
+    for flight_info, destination_info, direction_info in zip(flight_times, destination_names, direction_names):
         i, hours, minutes = flight_info
         _, municipality = destination_info
+        _, dir = direction_info
 
         print(f"{options:02d}         {hours:02d}:{minutes:02d}     {municipality:<20s}       Cost{i:02d}     "
-              f"Direction{i:02d}     Range{i:02d}")
+              f"{dir:<10s}     Range{i:02d}")
 
         options += 1
 
 
 flight_timetable()
-# airport_names()
-# print(get_some_airports())
-# airport = get_some_airports()
-# print((draw_airports_from_origin(34, 130)))
-# print(airport[6]["name"])
+#airport_names()
