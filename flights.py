414ce562e143502184fa72a1f998a5101be6a327
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2024-03-07 14:21:23 +0200

    simplified some code

--- flights.py
+++ flights.py
@@ -10,12 +10,13 @@ from game import game_controller
 # The return value is something like [{'flight_direction': 'North',
 # 'distance': 521, 'airport': {...}, 'time': (21, 35)}, ...]
 # Time is a tuple with ints: (hours, minutes)
-def sort_by_time(flight):   # Sort function for flight times
+def sort_by_time(flight):  # Sort function for flight times
     return flight['time']
+
 
 def times_of_the_flights(flights):  # Draws random times for flights and adds them to the list.
     # Assigns an index to randomly generated times. Index starts from 1 instead of 0.
-    for i, time in enumerate(range(16)):
+    for i in range(16):
         random_hours = random.randint(00, 23)
         random_minutes = random.randint(00, 59)
         flights[i]["time"] = [random_hours, random_minutes]
@@ -26,19 +27,19 @@ def times_of_the_flights(flights):  # Draws random times for flights and adds th
 
 
 def airport_type(airport_types):
-    port_names = []
+    port_types = []
 
     for airport in airport_types:  # Simplifies the names for airport types
         a_port_type = airport["type"]
         if a_port_type == "small_airport":
-            port_names.append("Small")
+            port_types.append("Small")
         elif a_port_type == "medium_airport":
-            port_names.append("Medium")
+            port_types.append("Medium")
         elif a_port_type == "large_airport":
-            port_names.append("Large")
+            port_types.append("Large")
         else:
-            port_names.append("Unknown")
-    return port_names
+            port_types.append("Unknown")
+    return port_types
 
 
 def flight_timetable():  # Prints a flight timetable with options for the player.
@@ -46,7 +47,7 @@ def flight_timetable():  # Prints a flight timetable with options for the player
     print(f"Options    Time      Destination              Airport(type)    "
           f"Direction           Distance         Cost")
 
-    # Creates a separate counter for Player Options.
+    # Creates a separate counter for flight options.
     options = 1
 
     # Randomly picks 16 flights from given coordinates (latitude and longitude)
@@ -56,6 +57,7 @@ def flight_timetable():  # Prints a flight timetable with options for the player
 
     # times_of_the_flights function uses the random flights to generate timetables for the flights.
     timed_flights = times_of_the_flights(random_flights)
+    # print(timed_flights)
     # Searches information about the flights from lists compiled in above functions and prints them like a timetable
     for i in range(0, len(timed_flights)):
         hours, minutes = timed_flights[i]["time"]
@@ -63,11 +65,11 @@ def flight_timetable():  # Prints a flight timetable with options for the player
         direction = timed_flights[i]["flight_direction"]
         distance = timed_flights[i]["distance"]
         cost = timed_flights[i]["cost"]
-        simplified = airport_type([timed_flights[i]["airport"]])
-        simplified_names = simplified[0]
+        simplified_types = airport_type([timed_flights[i]["airport"]])
+        types = simplified_types[0]
 
         print(
-            f"{options:02d}         {hours:02d}:{minutes:02d}     {municipality:<20s}     {simplified_names:<17s}"
+            f"{options:02d}         {hours:02d}:{minutes:02d}     {municipality:<20s}     {types:<17s}"
             f"{direction:<15s}     {distance:04d}km           {cost:.02f}€")
 
         options += 1
