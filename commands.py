e0c35f661af414d162f3d30a37da40787a98d8e2
Timppav <tino.vuorela@gmail.com>
2024-03-07 10:05:19 +0200

    Cleaned up some code, "fly" now prints out the timetables from flights.py

--- commands.py
+++ commands.py
@@ -1,22 +1,22 @@
 from colorama import Fore, Style
-from db import draw_airports_from_origin
+from flights import flight_timetable
 
 
 # Function that checks whether the given command is found in the list of commands, and then returns the
-# corresponding text or function
+# corresponding function
 def command(text):
-    textsplit = text.split()
-    if len(textsplit) == 1 and text in command_functions:
+    if text in command_functions:
         return command_functions[text]
-    elif len(textsplit) == 2 and textsplit[0] == "help":
-        return helphelp(textsplit[1])
     else:
         return invalid_command
 
 
-def helphelp(h):
-    if h in help_list:
-        print(help_list[h])
+# Prints the description for specified command
+def command_description(help_command):
+    if help_command in help_list:
+        print(help_list[help_command])
+    else:
+        return invalid_command()
 
 
 # Prints the description for every command
@@ -30,25 +30,24 @@ def print_status():
     print(status)
 
 
-# Testing a function that handles changing the players' location
-# The updated location should show up when using the "status" command
-# Doesn't work right now
+# Uses function imported from flights.py that prints the flights' timetable
+# TO DO: Using this command lets the player select a flight, changing the current location to a new one
 def fly():
-    # Function imported from db.py. Returns 16 airports from selected latitude and longitude
-    list_of_airports = draw_airports_from_origin(34, 130, "")
-    airport_num = 1
-    airport_dict = {}
-    # Lists every airport available, and assigns a number to each one
-    for airport in list_of_airports:
-        print(airport_num, airport)
-        airport_dict[airport_num] = airport
-        airport_num += 1
-    new_location = int(input("\nSelect the flight you want to take (1-16): "))
-    if new_location in airport_dict.keys():
-        status[2] = f"Airport {new_location}"
-        print(f"Location changed to {new_location}")
-    else:
-        print("Flight not found!")
+    flight_timetable()
+    print(Style.RESET_ALL)
+    # list_of_airports = draw_airports_from_origin(34, 130, "")
+    # airport_num = 1
+    # airport_dict = {}
+    # for airport in list_of_airports:
+    #     print(airport_num, airport)
+    #     airport_dict[airport_num] = airport
+    #     airport_num += 1
+    # new_location = int(input("\nSelect the flight you want to take (1-16): "))
+    # if new_location in airport_dict.keys():
+    #     status[2] = f"Airport {new_location}"
+    #     print(f"Location changed to {new_location}")
+    # else:
+    #     print("Flight not found!")
 
 
 def invalid_command():
@@ -63,8 +62,7 @@ help_list = {"help": f"{Fore.GREEN}Help{Style.RESET_ALL} - Shows this list. Typi
              "fly": f"{Fore.GREEN}FLy{Style.RESET_ALL} - Fly to specified airport",
              "exit": f"{Fore.GREEN}Exit{Style.RESET_ALL} - Quits the game"}
 
-# Player stats: 1 = Player name, 2 = Location, 3 = Money, 4 = CO2, 5 = Day, 6 = Hours, 7 = Minutes
-# The hours and minutes can probably be done a lot better
+# Player stats placeholder: 1 = Player name, 2 = Location, 3 = Money, 4 = CO2, 5 = Day, 6 = Hours, 7 = Minutes
 status = {1: "Player", 2: "Location", 3: 0,
           4: 0, 5: 0, 6: 0, 7: 0}
 
@@ -76,11 +74,11 @@ command_functions = {"help": print_helplist, "status": print_status, "fly": fly}
 while True:
     command_input = input("\nEnter a command: ").lower()
     inputsplit = command_input.split()
-    if len(inputsplit) == 1:
+    if command_input == "exit":
+        break
+    elif len(inputsplit) == 1:
         command(command_input)()
     elif len(inputsplit) == 2:
-        command(command_input)
-    elif command_input == "exit":
-        break
+        command_description(inputsplit[1])
     else:
         invalid_command()
