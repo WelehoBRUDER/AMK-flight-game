fb50c56040467bd1af6da3df485f4f6293b52516
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2024-03-12 15:47:58 +0200

    FIX INDEX ERROR!

--- game.py
+++ game.py
@@ -187,7 +187,7 @@ def init_game():
     for i in range(1, players_amount + 1):
         game_controller.add_player(i, start_cords)
 
-        print(f"Player {i} is now known as {game_controller.players[i].get_name()}")
+        print(f"Player {i} is now known as {game_controller.players[i - 1].get_name()}")
 
 
 def calc_cost(distance_amount):
