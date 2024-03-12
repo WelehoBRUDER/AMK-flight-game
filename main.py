ee4ea26893eeb5aa9f0690d7996f69db5af6452b
Timppav <tino.vuorela@gmail.com>
2024-03-12 19:57:53 +0200

    Removed unnecessary code

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-import time
-import db
-from colorama import Fore
 from game import *
 import commands
-import game
 import main_menu_and_leaderboards_test
-
-
-class Colors:
-    MAGENTA = '\033[95m'
-    CYAN = '\033[96m'
-    RESET = '\033[0m'
-
-
-# while True:
-#     player_name = input("Enter player name (or 'exit' to stop): ")
-#
-#     if player_name.lower() == 'exit':
-#         break
-#
-#     players.append(player_name)
-#
-# print("Player names:", players)
 
 
 def main():
