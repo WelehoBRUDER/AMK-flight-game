fa30705c673460c527c1c0a94c94100120177fb3
Timppav <tino.vuorela@gmail.com>
2024-03-13 05:40:09 +0200

    The 'fly' function now properly loops until a correct input is given. Added new option to cancel.

--- commands.py
+++ commands.py
@@ -40,14 +40,14 @@ def print_instructions():
                     "around the world in 20 days while trying to keep your expenses and CO2 emissions "
                     "as low as possible.",
                     "- The amount of days you have and your starting money is determined by the game difficulty.",
-                    "- The game can be played alone or with friends (turn based)."
+                    "- The game can be played alone or with friends (turn based).",
                     "- You will start on a random airport, and you will have to catch flights around the world until "
                     "you have circled around the globe.",
-                    f"-Airports can be {Fore.CYAN}small{Fore.RESET}, {Fore.BLUE}medium{Fore.RESET} or "
+                    f"- Airports can be {Fore.CYAN}small{Fore.RESET}, {Fore.BLUE}medium{Fore.RESET} or "
                     f"{Fore.MAGENTA}large{Fore.RESET}. How far you can fly will depend on the size of the airport.",
-                    f"-Typing {Fore.GREEN}'help'{Fore.RESET} (without apostrophes) will display a list of "
+                    f"- Typing {Fore.GREEN}'help'{Fore.RESET} (without apostrophes) will display a list of "
                     f"available commands and a short description for each one.",
-                    f"-Typing {Fore.RED}'exit'{Fore.RESET} at any point will quit the game.",
+                    f"- Typing {Fore.RED}'exit'{Fore.RESET} at any point will quit the game.",
                     "Good luck!"]
     for line in instructions:
         print(line)
@@ -62,6 +62,7 @@ def print_status():
     print(current_player.origin_latitude, current_player.origin_longitude)
     start_airport = db.get_airport_by_cords(current_player.origin_latitude, current_player.origin_longitude)
     print(start_airport)
+    # Creates a table, defines the column names, and displays player data on the second first row
     console = Console()
     table = Table(show_header=True, header_style="cyan")
     table.add_column("Name", style="white")
@@ -78,33 +79,39 @@ def print_status():
                    f"{current_player.distance_traveled}km",
                    f"{current_player.get_time()}",
                    f"{current_player.halfway_latitude, current_player.halfway_longitude}", f"{start_airport['name']}"))
-
     console.print(table)
 
 
 # Uses function imported from flights.py that prints the flights' timetable
-# Lets the player choose a flight, changing their current location to a new one)
+# Lets the player choose a flight, changing their current location to a new one and ending their turn
 def fly():
     current_player = game.game_controller.get_current_player()
-    flights.flight_timetable()
-    try:
-        selection = input(Fore.RESET + "\nSelect where you want to fly (1-16): ")
-        game.clear_and_exit_check(selection)
-        selection = int(selection)
-        if 0 < selection < 17:
-            chosen_flight = (flights.timed_flights[selection - 1])
-            port = chosen_flight["airport"]
-            current_player.fly(chosen_flight)
-            print(f"\nWelcome to {Fore.CYAN}{port["name"]}{Fore.RESET}!")
-        else:
-            print("Invalid selection!")
-    except ValueError:
-        print("Selection must be a number!")
+    current_airport = db.get_airport(current_player.location)
+    airport_name = current_airport["name"]
+    while True:
+        print(f"Current location: {Fore.CYAN}{airport_name}{Fore.RESET}\n")
+        flights.flight_timetable()
+        try:
+            selection = input(Fore.RESET + "\nSelect where you want to fly (1-16) (0 to cancel): ")
+            game.clear_and_exit_check(selection)
+            selection = int(selection)
+            if 0 < selection < 17:
+                chosen_flight = (flights.timed_flights[selection - 1])
+                port = chosen_flight["airport"]
+                current_player.fly(chosen_flight)
+                print(f"\nWelcome to {Fore.CYAN}{port["name"]}{Fore.RESET}!")
+                break
+            elif selection == 0:
+                return
+            else:
+                print(f"{Fore.RED}Invalid selection!{Fore.RESET}")
+        except ValueError:
+            print(f"{Fore.RED}Selection must be a number!\n{Fore.RESET}")
     return "break"
 
 
 def invalid_command():
-    print("Invalid command!")
+    print(f"{Fore.RED}Invalid command!{Fore.RESET}")
 
 
 # Keeps asking the player for a command until they end the turn or quit the game
@@ -126,7 +133,7 @@ def run_commands():
 # A dictionary of all commands and a short explanation for each one
 help_list = {"help": f"{Fore.GREEN}Help{Fore.RESET} - Shows this list. Typing a command after "
                      f"'help' will give you the description for that specific command.",
-             "instructions": f"{Fore.GREEN}Instructions{Fore.RESET} - Shows the instructions for playing the game",
+             "instructions": f"{Fore.GREEN}Instructions{Fore.RESET} - Shows the instructions for playing the game.",
              "status": f"{Fore.GREEN}Status{Fore.RESET} - "
                        f"Shows your name, location, money, consumed CO2, days and time.",
              "fly": f"{Fore.GREEN}Fly{Fore.RESET} - Displays all available flights, and lets you pick one of them.",
