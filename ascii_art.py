2d12c55f5456e18f3a54b5d342c904c6ff7edcdd
Timppav <tino.vuorela@gmail.com>
2024-03-13 18:18:38 +0200

    Added new file 'ascii_art' and moved all ascii art into it.

---
+++ ascii_art.py
@@ -0,0 +1,50 @@
+from colorama import Fore
+import time
+import sys
+import game
+
+
+def ascii_plane():
+    print(r"""
+    __  _
+    \ `/ |
+     \__`!
+     / ,' `-.__________________
+    '-'\_____                LI`-.
+       <____()-=O=O=O=O=O=[]====--)
+         `.___ ,-----,_______...-'
+              /    .'
+             /   .'
+            /  .'         
+            `-'
+    """)
+
+
+def game_title(check):
+    ascii_art = [Fore.MAGENTA + "\n       █     ████     ███    █   █   █   █   ███        █████   █   █   █████ \n",
+                 "      █ █    █   █   █   █   █   █   ██  █   █  █         █     █   █   █     \n",
+                 "     █████   █████   █   █   █   █   █ █ █   █   █        █     █████   █████ \n",
+                 "     █   █   █  █    █   █   █   █   █  ██   █  █         █     █   █   █     \n",
+                 "     █   █   █   █    ███     ███    █   █   ███          █     █   █   █████ \n\n",
+                 "                  █   █   █    ███    ████    █       ███                     \n",
+                 "                  █   █   █   █   █   █   █   █       █  █                    \n",
+                 "                   █ █ █ █    █   █   █████   █       █   █                   \n",
+                 "                   █ █ █ █    █   █   █  █    █       █  █                    \n",
+                 "                    █   █      ███    █   █   █████   ███      \n" + Fore.RESET]
+
+    # Prints the title for the main menu
+    if check == 1:
+        for line in ascii_art:
+            for letter in line:
+                print(letter, end="")
+                sys.stdout.flush()
+        print("\n-----------------------------------------------------------------------------------\n")
+    # Prints the title for the intro
+    else:
+        for line in ascii_art:
+            for letter in line:
+                print(letter, end="")
+                time.sleep(0.002)
+                sys.stdout.flush()
+        input(f"{Fore.BLUE}\nInput anything to continue: {Fore.RESET}")
+        game.clear_and_exit_check(0)
